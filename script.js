33c43a3bdd438d3e335a3e02aa09d172a5d7d2a1
jesusnboone <jesusnboone@gmail.com>
2020-06-05 03:01:28 -0500

    added arrays, created variables

--- script.js
+++ script.js
@@ -14,10 +14,42 @@ function generatePassword() {
   
   if (pass >=8 && pass < 128) {
   var spechar = confirm("Would you like to include special characters?");
-  var 
+  var num = confirm("Would you like to include numbers?");
+  var lowerchar = confirm("Would you like to include lowercase letters?");
+  var upperchar = confirm("Would you like to include uppercase characters?");
   }
 
+  if (spechar) {
+    var spechar = ["!","@","#","$","%","^","&","*","(",")","-","_","+","{","}","[","]"];
+  }
+
+  if (num) {
+    var num = [0,1,2,3,4,5,6,7,8,9]
+  }
   
+  if (lowerchar) {
+    var lowerchar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  }
+
+  if (upperchar) {
+    var upperchar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  }
+
+  var specharrandom = spechar[Math.floor(Math.random() * spechar.length)];
+  var numrandom = num[Math.floor(Math.random() * num.length)];
+  var lowercharrandom = lowerchar[Math.floor(Math.random() * lowerchar.length)];
+  var uppercharrandom = upperchar[Math.floor(Math.random() * upperchar.length)];
+
+  console.log(specharrandom);
+  console.log(numrandom);
+  console.log(lowercharrandom);
+  console.log(uppercharrandom);
+
+  var passwordgen = [specharrandom,numrandom,lowercharrandom,uppercharrandom]
+
+  var passwordgenfinal = passwordgen[Math.floor(Math.random() * passwordgen.length)];
+
+  return passwordgenfinal
 }
 
 
