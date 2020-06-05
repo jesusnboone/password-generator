6c36705192f5576c18ca651e4c944685231ba110
jesusnboone <jesusnboone@gmail.com>
2020-06-05 08:50:04 -0500

    overhauled everything

--- script.js
+++ script.js
@@ -6,53 +6,46 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword() {
-  var pass = prompt("How long would you like your password to be? (hint: 8-128)");
+  var passLength = prompt("How long would you like your password to be? (hint: 8-128)");
 
-  if (pass < 8 || pass > 128 ) {
+  if (passLength < 8 || passLength > 128 ) {
     alert("Please enter a character length that is between 8 and 128!");
   }
   
-  if (pass >=8 && pass < 128) {
+  if (passLength >=8 && passLength < 128) {
   var spechar = confirm("Would you like to include special characters?");
   var num = confirm("Would you like to include numbers?");
   var lowerchar = confirm("Would you like to include lowercase letters?");
   var upperchar = confirm("Would you like to include uppercase characters?");
   }
-
-  if (spechar) {
-    var spechar = ["!","@","#","$","%","^","&","*","(",")","-","_","+","{","}","[","]"];
-  }
-
-  if (num) {
-    var num = [0,1,2,3,4,5,6,7,8,9]
-  }
   
-  if (lowerchar) {
-    var lowerchar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var passLength=passLength;
+
+  if (spechar === true && num === true && lowerchar === true && upperchar === true) {
+  var criteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
   }
 
-  if (upperchar) {
-    var upperchar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  if (spechar === false && num === true && lowerchar === true && upperchar === true) {
+    var criteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  }
+  if (spechar === false && num === false && lowerchar === true && upperchar === true){
+    var criteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   }
 
-  var specharrandom = spechar[Math.floor(Math.random() * spechar.length)];
-  var numrandom = num[Math.floor(Math.random() * num.length)];
-  var lowercharrandom = lowerchar[Math.floor(Math.random() * lowerchar.length)];
-  var uppercharrandom = upperchar[Math.floor(Math.random() * upperchar.length)];
+  if (spechar === false && num === false && lowerchar === false && upperchar === true) {
+    var criteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
 
-  var ranchargen = [specharrandom,numrandom,lowercharrandom,uppercharrandom];
+  if (spechar === false && num === false && lowerchar === false && upperchar === false) {
+    var criteria = ""
+  }
+  var password = "";
 
-  var trueranchar = ranchargen[Math.floor(Math.random() * ranchargen.length)];
+  for (var i=0; i<=passLength; i++) {
+    password = password + criteria.charAt(Math.floor(Math.random() * Math.floor(criteria.length - 1)));
+  }
 
-  console.log(trueranchar)
-  console.log(trueranchar)
-  console.log(trueranchar)
-  console.log(trueranchar)
-
-  return trueranchar
-
-
-
+  return password
 }
 
 
