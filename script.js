eb0e71b78c40f9dc052c24346435c180470ac830
jesusnboone <jesusnboone@gmail.com>
2020-06-05 01:39:03 -0500

    added pass length requirement

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+//don't delete
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+function generatePassword() {
+  var pass = prompt("How long would you like your password to be? (hint: 8-128)");
+
+  if (pass < 8 || pass > 128 ) {
+    alert("Please enter a character length that is between 8 and 128!");
+  }
+  
+  if (pass >=8 && pass < 128) {
+  var spechar = confirm("Would you like to include special characters?");
+  var 
+  }
+
+  
+}
+
+
+//don't delete
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,5 +30,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+//don't delete
 generateBtn.addEventListener("click", writePassword);
